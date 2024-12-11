3d3316e39b54cae9dc707a082514ff47d0be9ebd
Musa Hoosain <musahoosain@gmail.com>
2024-12-11 14:03:56 -0500

    finished ex 3

--- NorthwindTradersSpringBoot/src/main/java/com/pluralsight/NorthwindTradersSpringBoot/NorthwindTradersSpringBootApplication.java
+++ NorthwindTradersSpringBoot/src/main/java/com/pluralsight/NorthwindTradersSpringBoot/NorthwindTradersSpringBootApplication.java
@@ -12,58 +12,8 @@ import java.util.List;
 @SpringBootApplication
 public class NorthwindTradersSpringBootApplication {
 
-	private static ProductDAO productDAO;
-	private static ApplicationContext appContenxt;
-
 	public static void main(String[] args) {
-
-		appContenxt = SpringApplication.run(NorthwindTradersSpringBootApplication.class, args);
-
-		for (String bean : appContenxt.getBeanDefinitionNames()){
-			System.out.println(bean);
-		}
-
-		productDAO = appContenxt.getBean(ProductDAO.class);
-
-		String options = """
-                Please select from the following choices:
-                1 - Add Product
-                2 - List All Products
-                99 - Quit
-
-                >>>\s""";
-
-		int selection;
-
-		// User Interface Loop
-		do {
-			selection = Console.PromptForInt(options);
-			switch (selection) {
-				case 1 -> processAddProduct();
-				case 2 -> processGetAllProducts();
-				case 99 -> System.exit(0);
-				default -> System.out.println("Invalid selection. Please try again.");
-			}
-		} while (true);
+		SpringApplication.run(NorthwindTradersSpringBootApplication.class, args);
 
 	}
-
-	private static void processGetAllProducts() {
-		List<Product> products = productDAO.getAll();
-
-		for(Product p : products){
-			System.out.println(p);
-		}
-	}
-
-	private static void processAddProduct() {
-		int productId = Console.PromptForInt("Please enter the Product ID: ");
-		String name = Console.PromptForString("Please enter the Product Name: ");
-		String category = Console.PromptForString("Please enter the Category: ");
-		double price = Console.PromptForDouble("Please enter the price: ");
-		Product p1 = new Product(productId, name, category, price);
-
-		productDAO.add(p1);
-	}
-
 }
